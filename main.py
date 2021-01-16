a3e11e67b12ac3918b048290000f129228815b68
HCEG6 <louis159@naver.com>
2021-01-16 23:14:08 +0900

    Ver 1.1

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from server.db import dbModule as db
 from sentence_transformers import SentenceTransformer, util
+import scipy.stats
 
 
 token = ''
@@ -195,51 +196,70 @@ def question_answer(query_input):
     # query embedding
     query_embedding = embedder.encode(query, convert_to_tensor=True)
 
-    # cosine_similarity 계산
-    cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings_qa)[0]
+    # Spearman 상관계수가 0.3보다 높은 값의 내용만 얻음
+    Top_Context = []
 
-    # We use np.argpartition, to only partially sort the top_k results
+    for idx in range(len(corpus_embeddings_ig)):
+        spearmanr = scipy.stats.spearmanr(query_embedding, corpus_embeddings_qa[idx])
+        if abs(spearmanr[0]) > 0.3:
+            Top_Context.append([spearmanr[0], idx])
+
+    # 상위부터 정렬
+    Top_Context = sorted(Top_Context, reverse=True)
+
     top_k = 5
-    top_results = np.argpartition(-cos_scores, range(top_k))[0:top_k]
 
     read_text_to_json = json.dumps(attachment_answer_json)
     read_json = json.loads(read_text_to_json)
-    for i, idx in enumerate(top_results[0:top_k]):
-        temp_str = str(i+1) + "번 " + corpus_qa[idx].strip()
-        temp_str = temp_str[0:30] + "(%2.f%%)" % (cos_scores[idx]*100)
+
+    for i in range(top_k):
+        temp_str = str(i + 1) + "번 " + corpus_qa[Top_Context[idx][1]].strip()
+        temp_str = temp_str[0:30] + "(%2.f%%)" % (Top_Context[idx][0]*100)
         read_json["blocks"][0]['elements'][0]['options'][i]['text']['text'] = temp_str
+
     return read_json
 
 
 def im_ground(query_input):
     query = delete_exclamation_mark(query_input)
 
-    # query embedding
+    # query_embedding
     query_embedding = embedder.encode(query, convert_to_tensor=True)
 
-    # cosine_similarity 계산
-    cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings_ig)[0]
+    # Spearman 상관계수가 0.3보다 높은 값의 내용만 얻음
+    Top_Context = []
 
-    # 상위 다섯개 출력 (10개 여분으로 세팅하고, 중복 제거)
+    for idx in range(len(corpus_embeddings_ig)):
+        spearmanr = scipy.stats.spearmanr(query_embedding, corpus_embeddings_ig[idx])
+        if abs(spearmanr[0]) > 0.3:
+            Top_Context.append([spearmanr[0], idx])
 
-    top_k = 10
-    top_results_ig = np.argpartition(-cos_scores, range(top_k))
+    #상위부터 정렬
+    Top_Context = sorted(Top_Context, reverse=True)
 
     name_overlap = []
 
     number = 1
-    result =''
-    for i, idx in enumerate(top_results_ig[0:top_k]):
+    result = ""
+
+    # 상위 5개 출력 (중복제거)
+
+    for idx in range(len(Top_Context)):
         if number > 5:
             break
-        if cos_scores[idx] > 0.5:
-            if not name_ig[idx] in name_overlap:
-                temp = str(i + 1) + "번 " + name_ig[idx].strip() + ', ' + corpus_ig[idx].strip() + '\n'
-                result = result + str(temp)
-                name_overlap.append(name_ig[idx].strip())
-                number += 1
+        if not name_ig[idx] in name_overlap:
+            temp = str(idx + 1) + "번" + name_ig[Top_Context[idx][1]].strip() + ", " + corpus_ig[Top_Context[idx][1]].strip() + "\n"
+            result = result + str(temp)
+            name_overlap.append(name_ig[Top_Context[idx][1]].strip())
+            number += 1
 
-    return result
+            """print("유사도 : ", Top_Context[idx][0])
+            print("이름 : ", name[Top_Context[idx][1]])
+            print("내용 : ", corpus[Top_Context[idx][1]])
+            name_overlap.append(name[Top_Context[idx][1]].strip())
+            number += 1"""
+
+        return result
 
 
 @app.route("/slack", methods=["GET", "POST"])
